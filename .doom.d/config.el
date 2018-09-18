f986c803343b5cbaea73098782503b068e8f597d
tunc <tuncozanaydin@gmail.com>
2018-09-18 09:24:38 +0200

    epa-file-enable is not required anymore apparently

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,27 +3,9 @@
 (setq doom-font (font-spec :family "Source Code Pro" :size 15))
 (setq doom-theme 'doom-one)
 
-(setq evil-want-integration nil)
-;;(after! org
-;;  (set-face-attribute 'org-level-1 nil :height 1.0))
-
-;;(eval-after-load "org"
-;;  (set-face-attribute 'org-level-1 nil :height 1.0))
-;;(add-hook 'org-mode-hook (set-face-attribute 'org-level-1 nil :height 1.0))
-
-;; (map! "C-;" 'comcent-line)
-;; (map! "C-s" 'save-buffer)
-
 (require 'company)
 (setq company-idle-delay 0.2
       company-minimum-prefix-length 3)
-
-;; (defun my/python-mode-hook ()
-;;   (add-to-list 'company-backends 'company-jedi))
-
-;; (add-hook 'python-mode-hook 'my/python-mode-hook)
-
-(epa-file-enable)
 
 ;; (require 'realgud)
 (after! realgud
