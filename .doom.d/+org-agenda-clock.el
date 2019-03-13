a1a56f58ddb98d6a4580c398f133bee9e10c3a30
tunc <tuncozanaydin@gmail.com>
2019-03-13 16:56:40 +0100

    Org-clock config

---
+++ .doom.d/+org-agenda-clock.el
@@ -0,0 +1,6 @@
+;;; ~/.dotfiles/.doom.d/+org-agenda-clock.el -*- lexical-binding: t; -*-
+
+(org-clock-persistence-insinuate)
+(setq org-clock-persist-query-resume nil)
+(setq org-clock-persist t)
+(setq org-duration-format (quote h:mm))
